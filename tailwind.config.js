af29cb4fbff76b8e76b296aac4a055500b38ad62
AmirHoseinAshkani <amirhoseinashkaniofficial@gmail.com>
2025-01-11 18:49:49 +0330

    Articles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,53 @@
 module.exports = {
   content: ["./Src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    
-    extend: {
 
-      fontFamily:{
-        Vazirmatn:["vazirmatn"]
+    extend: {
+      container:{
+        center:true,
+        padding:'1rem',
       },
 
+      fontFamily: {
+        Vazirmatn: ["vazirmatn"],
+      },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [
+    [require("flowbite/plugin")],
+
+    function ({ addComponents }) {
+
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+
+          "@screen sm": {
+            maxWidth: "540px",
+          },
+
+          "@screen md": {
+            maxWidth: "720px",
+          },
+
+          "@screen lg": {
+            maxWidth: "960px",
+          },
+
+          "@screen xl": {
+            maxWidth: "1140px",
+          },
+
+          "@screen 2xl": {
+            maxWidth: "1300px",
+          },
+
+        },
+
+      });
+
+    },
+
+  ],
+  
 };
